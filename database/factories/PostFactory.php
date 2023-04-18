7e45a73b69a87e97d2d1aa7961df09c8008d8e9a
Xandervdb1 <xandervandenbossche0@gmail.com>
2023-04-18 10:14:42 +0200

    Add factory migration and seeder

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+use App\Models\User;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        $title = fake()->words(3, true);
+        $slug = Str::slug($title);
+        return [
+            'title' => $title,
+            'short_desc' => fake()->sentence(),
+            'slug' => $slug,
+            'description' => fake()->text(),
+            'tag1_id' => rand(1, 5),
+            'tag2_id' => rand(1, 5),
+            'user_id' => User::all()->random()->id,
+        ]; 
+    }
+}
